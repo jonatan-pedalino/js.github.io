c4903e98128eb42ed28579a5a5cdc56a3a980a2a
jonatanpedalino133 <159931278+jonatan-pedalino@users.noreply.github.com>
2024-05-07 19:28:15 -0300

    i

---
+++ scrips.js
@@ -0,0 +1,98 @@
+/*
+const passUsuario = "jona89"
+const passContraseña = "123456"
+
+let passWord = prompt ("ingrese su usuario")
+
+if (passUsuario == passWord){
+    alert ("su usuario es correcto")
+}
+
+else {
+    alert ("su usuario es incorrecto")
+}
+
+let passPassword = prompt ("ingrese su contraseña")
+
+if (passContraseña == passPassword) {
+    alert ("puede ingresar")
+}
+else ("su contraseña es incorrecta")
+*/
+/*
+const dia = prompt("ingrese el dia")
+
+switch(dia){
+    case "1":
+        alert("es lunes")
+        break
+    case "2":
+        alert("es martes")
+        break
+    case "3":
+        alert("es miercoles")
+        break
+    case "4":
+        alert("es jueves")
+        break
+    default:
+        alert("no ingreso nada")
+        break
+}
+*/
+/*
+
+fc = ((a) => a + 100)
+
+console.log( fc(10) )
+
+const passUsuario = "jona89"
+const passContraseña = "123456"
+
+let passPassword = prompt ("ingrese su usuario")
+    if (passUsuario == passPassword){
+        let passWord = prompt ("ingrese su contraseña")
+        if (passWord == passContraseña){
+            alert ("puedes ingresar")
+        }
+    } else {
+        alert ("su usuario es incorrecto")
+        
+    }
+
+let saldoCajero = 20000
+let saldoUsuario = 87000
+
+const opcionCajero = prompt ("ingreso al cajero... ingrese una opcion: 1:consultar saldo 2:extraer dinero 3:pedir un crediro")
+
+switch(opcionCajero){
+    case "1":
+            alert ("su saldo es " + saldoUsuario)
+                prompt ("ingrese 1 si quiere sacar dinero ingrese 2 para regresar al menu")
+                    case "1":
+                    if (saldoCajero > 0){
+                        console.log ("contiene dinero")
+                    } else {
+                        alert ("este cajero no posee dinero")
+                        break
+                    }
+                    var sacarPlata = Number(prompt ("ingrese el monto a retirar"))
+                    if (saldoCajero >= sacarPlata){
+                        saldoUsuario = saldoUsuario - sacarPlata 
+                        alert ("su saldo actual es " + saldoUsuario)
+                        saldoCajero = saldoCajero - sacarPlata
+                    } else {
+                        alert ("este cajero no posee el monto suficiente")
+                    }
+                    case "2": 
+
+    case "2":
+        var sacarPlata = Number( prompt ("ingrese el monto a retirar"))
+                    if (saldoCajero > 0){
+                        console.log ("contiene dinero")
+                    } else {
+                        alert ("este cajero no posee dinero")
+                        break
+                    }            
+}
+*/
